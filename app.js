0d2a305cc9bc7936f6f1bcd1e4a8d393dab4c661
CHANGWOOA <speed5312@naver.com>
2024-12-04 15:18:00 +0900

    BE 기본 틀 추가 수정 작업 및 리머지 작업 완료

--- app.js
+++ app.js
@@ -3,14 +3,8 @@ const path = require("path");
 
 const app = express();
 const router = require("./src/router/router")
-app.use("/", router);
 
-app.get("/login", () => {
-    res.render("login");
-})
-app.get("/logout", () => {
-    res.render("logout");
-})
+
 
 app.set("views", "./src/views");
 app.set("view engine", "ejs");
