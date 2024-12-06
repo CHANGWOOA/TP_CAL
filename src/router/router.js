cd34aa31e27c87bb4b09f76685aaa6151368099d
harvoid <ky0915111@naver.com>
2024-12-06 14:12:17 +0900

    라우터 오류 수정

--- src/router/router.js
+++ src/router/router.js
@@ -4,69 +4,6 @@ module.exports=(app)=>{
     app.use(bodyParser.urlencoded())
     const memberRouter = require("./member/member_router")
     app.use("/member", memberRouter)
-
-    router.get('/',(req,res)=>{
-        res.render('login')
-    });
-
-<<<<<<< HEAD
-
-    const todolist=[
-        {title:'titletitle',
-            memo:'memomemomemo',
-            date:'2024-07-12'
-        },
-        {title:'titletitle',
-            memo:'memomemomemo',
-            date:'2024-08-12'
-        },
-        {title:'titletitle',
-            memo:'memomemomemo',
-            date:'2024-08-12'
-        },
-        {title:'titletitle',
-            memo:'memomemomemo',
-            date:'2024-08-12'
-        },
-        {title:'titletitle',
-            memo:'memomemomemo',
-            date:'2024-08-12'
-        },
-        {title:'titletitle',
-            memo:'memomemomemo',
-            date:'2024-09-12'
-        }
-    ];
-
-    const boardList=[
-        {
-            title:'제목제목제목1',
-            url:'aaaa'
-        },        {
-            title:'제목제목제목2',
-            url:'aaaa'
-        },
-        {
-            title:'제목제목제목3',
-            url:'aaaa'
-        },
-        {
-            title:'제목제목제목4',
-            url:'aaaa'
-        }
-    ];
-    router.post('/main',(req,res)=>{
-        res.render('index',{todolist:todolist,boardList:boardList})
-=======
-    router.get('/main',(req,res)=>{
-        res.render('index')
->>>>>>> 9eae4303a864698d03ac71008ebede355ea53b1d
-    });
-
-    router.get('/main',(req,res)=>{
-        res.render('index',{todolist:todolist,boardList:boardList})
-
-    })
     
     router.get('/calendar',(req,res)=>{
         res.render('calendar')
@@ -81,6 +18,16 @@ module.exports=(app)=>{
         res.redirect('/');
     })
 
+    router.get('/',(req,res)=>{
+        res.render('login')
+    })
+
+    router.post('/main',(req,res)=>{
+        res.render('index',{todolist:todolist,boardList:boardList})
+    })
+
+    router.get('/main',(req,res)=>{
+        res.render('index',{todolist:todolist,boardList:boardList})
+    })
     return router;
 }
-
