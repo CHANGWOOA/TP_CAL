c7159e663a0ab3a7c69ff653d283c655b31d2c2b
aaa <aaa@aaa.com>
2024-12-09 17:21:07 +0900

    feat: board read 부분 코드 일부 수정

--- src/controller/board/board_ctrl.js
+++ src/controller/board/board_ctrl.js
@@ -9,7 +9,7 @@ const views={
         const data = await ser.boardRead.list ( req.query.start ) //http://...//list?start=10 ->이 경우req.query.start =10
         res.render("board/board", { list: data.list, start:data.start, page: data.page })
     
-        //console.log("b ctrl views:", data.list, data.start, data.page)
+        //console.log("b ctrl views:", data.list)
     
     }, //결과값을 data로 정의함
     writeForm : () => {
@@ -20,8 +20,8 @@ const views={
         const contentList = await ser.boardRead.detail (req.params.P_ID) //클릭 시 들어오는 params를 서비스로 전송하고
         //console.log("board ctrl:", contentList.rows)
         const data = contentList.rows //dao를 거쳐 온 P_ID글고유번호에 대한 해당 글의 정보 리스트를 배열형식으로 받아 data 변수에 저장
-        res.render("board/board_detail", { data }) //data를 ejs로 보낸다-> 반복문 사용해서 건들여야 한다고...
-        console.log("b ctrl detail:", data)
+        res.render("board/board_detail", { data }) //data를 ejs로 보낸다->ejs 사용하지 않고 data[0].P_ID 형식으로 바로 정보 받을 수 있음
+        //console.log("b ctrl detail:", data[0].P_ID)
     },
     modify : async (req, res) => {
         const data= await ser.boardRead.data(req,params.P_ID);
