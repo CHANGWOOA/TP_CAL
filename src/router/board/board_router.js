f5930ab4d2117b6c3ab5de363566e02eac858429
hoyoung <skghdud024@naver.com>
2024-12-09 13:32:35 +0900

    BE 병합

--- src/router/board/board_router.js
+++ src/router/board/board_router.js
@@ -1,7 +1,6 @@
 const router= require("express").Router();
 const ctrl= require("../../controller/board/board_ctrl")
 
-<<<<<<< HEAD
 router.post('/newboard',(req,res)=>{
     console.log('new baord data ',req.body);
     res.send('');
@@ -10,11 +9,8 @@ router.post('/newboard',(req,res)=>{
 
 router.get("/write_form", ctrl.views.writeForm);
 router.post("/write", ctrl.process.write);
-
-=======
 router.get("/delete/:P_ID/:file_name", ctrl.process.delete);
 router.get("/modify_form/:P_ID", ctrl.views.modifyForm);
 router.post("/modify", upload.single('file_name'), ctrl.process.modify);
->>>>>>> BE
 
 module.exports=router;
