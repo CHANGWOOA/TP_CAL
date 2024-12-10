317f8d1fb9c6bf9d692ead254560c7666751ff02
hoyoung <skghdud024@naver.com>
2024-12-10 15:45:00 +0900

    중간 저장

--- src/router/todo/todo_router.js
+++ src/router/todo/todo_router.js
@@ -8,38 +8,8 @@ router.post('/newtodo',(req,res)=>{
 });
 
 router.get('/',(req,res)=>{
-    // 예시??
-    const todolist=[];
-    const mem=[
-        '멤버1','멤버2','멤버3'
-    ]
-    const memtodo=[
-    
-        {
-            name:'멤버1',
-            todo: {
-                // todo 항목 : 완료 여부
-                '장 보기':'on',
-                '과제':'off'
-            }
-        },
-        {
-            name:'멤버2',
-            todo: {
-                '책장 청소':'off',
-                '병원 예약':'off'
-            }
-        },
-        {
-            name:'멤버3',
-            todo: {
-                '기말고사 준비':'off',
-                '강아지 산책':'on',
-                '온라인 강의':'on'
-            }
-        }
-    ]
-    res.render('todo/todo',{todolist:todolist,mem:mem,memtodo:memtodo})
+    const todolist=[]
+    res.render('todo/todo',{todolist:todolist})
 });
 
 module.exports=router;
