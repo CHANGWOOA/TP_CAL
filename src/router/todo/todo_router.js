4ad3d83ffa66763f5c3c04e83b01e7b33a376e4e
hoyoung <skghdud024@naver.com>
2024-12-11 12:39:38 +0900

    todo list 중간 저장

--- src/router/todo/todo_router.js
+++ src/router/todo/todo_router.js
@@ -3,15 +3,16 @@ const ctrl=require('../../controller/todo/todo_ctrl');
 
 router.post('/newtodo',(req,res)=>{
     console.log('new todo data ',req.body);
+    // let todo=ctrl.views.data();
+    // console.log('new todo todo ',todo);
     // res.render('todo/newtodo');
-    res.send('zz')
+    res.redirect('/todo')
 });
 
-router.get('/',(req,res)=>{
-    const todolist=[]
-    const memlist=['aaa','bbb','ccc','ddd','eee'];
-    const teamlist=[]
-    res.render('todo/todo',{todo:todolist,mem:memlist,team:teamlist})
+router.get('/',async(req,res)=>{
+    let todolist=await ctrl.views.data();
+    // console.log('zz',todolist);
+    res.render('todo/todo',{todo:todolist})
 });
 
 module.exports=router;
