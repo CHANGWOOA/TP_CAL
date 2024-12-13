c1115ff7f4380362650c651c175aabf303ce24cf
hoyoung <skghdud024@naver.com>
2024-12-13 14:31:38 +0900

    todo modify

--- src/controller/todo/todo_ctrl.js
+++ src/controller/todo/todo_ctrl.js
@@ -1,54 +1,49 @@
+const ser= require("../../service/todo/todo_service")
+const serBoard = require("../../service/board/board_service")
+const serCom= require("../../service/ser_common")
+const memSer = require("../../service/member/member_service")
+
 const views = {
-    list : () => {
+
+    list : async(req,res) => {
+        
+        //console.log('session',req.session)
+        const data= await ser.todoRead.list(req.session.username)
+        //console.log('todo ctrl',data.rows)
+        res.render("todo/todo",{ todo: data })
+        //data.rows에서 data로 수정하였습니다.
+        //밑에 data 함수와 return 값을 동일하게 지정해야
+        //todolist.ejs에서 같은 형식을 사용할 수 있습니다.
 
     },
-    writeForm : () => {
-
-    },
-    data : () => {
-        let todo=[
-            {
-                title:'공중 회전 제비 다섯 바퀴 연속 돌기',
-                priority: '3',
-                id:'useghr',
-                pk:'aaavv'
-            },
-            {
-                title:'샐러드 야채 빼고 먹기 협회 워크샵',
-                priority: '2',
-                id:'users',
-                pk:'aaahe'
-            },
-            {
-                title:'지구는 샌드위치다!! 동아리 정기모임',
-                priority: '1',
-                id:'useeer',
-                pk:'aaeywa'
-            },
-            {
-                title:' 홈프로텍트 주식회사 "white-hands"',
-                priority: '0',
-                id:'useeer',
-                pk:'aaeywa'
-            }
-        ];
-
-        return todo;
-
-    },
-    modifyForm : () => {
-
+    data: async(req,res)=>{
+        
+        //console.log("todoctrl session name", req.session )
+        //let user=req.session.username
+        const data= await ser.todoRead.list(req.session.username)
+        const boardList = await serBoard.boardRead.list(req.start)
+        // console.log('todo ctrl data',data.rows)
+        res.render('index',{todo:data, boardList:boardList})
     }
 }
 const process= {
-    write : () => {
-
+    write : async(req, res) => {
+        const msg= await ser.todoInsert.write(req.body, req.session.username);
+        res.send(msg)
     },
     delete : () => {
 
     },
     modify : () => {
 
+    },
+    pUpdate : async (req, res) => {
+        const result = await ser.todoUpdate.pUpdate (req.session.username)
+
+    },
+    complete : async (req, res) => {
+        const result = await ser.todoUpdate.complete (req.body, req.username)
+        res.send("업데이트 완료")
     }
 }
 module.exports= {views, process}
