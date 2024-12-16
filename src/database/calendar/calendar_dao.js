de9fa70e360c13a865302124b9fea0a37417bac1
miieun <jiguiiin@naver.com>
2024-12-16 09:01:04 +0900

    게시판 코드 작성중

---
+++ src/database/calendar/calendar_dao.js
@@ -0,0 +1,41 @@
+//const { autoCommit } = require("oracledb");
+const con = require("../db_common");
+
+const calRead={
+    data: async ( username ) => {//작성한 것 불러오기
+            const sql = `select * from CAL where U_ID = '${ username }'`;
+            let cal;
+            try {
+                cal =  await(await con).execute( sql ); 
+            } catch (err) {
+                console.log(err)
+            }
+            return cal;
+        }
+}
+const calInsert={
+    write: async(body, username)=>{//캘린더 작성
+        const sql= `INSERT INTO CAL (C_ID, U_ID, C_TITLE, C_CONTENT, C_STARTDATE, C_ENDDATE)
+        VALUES (cal_seq.nextval, '${username}', '${body.C_TITLE}','${body.C_CONTENT}', sysdate, sysdate)`
+
+        //console.log("cal dao", sql)
+        let result = 0;
+                try{
+                    result = await(await con).execute(sql);
+                }catch(err){
+                    console.log(err)
+                }
+                return result;
+    }
+}
+const calUpdate= {
+    delete : async(body)=>{
+        const sql= `delete from CAL where C_TITLE`='${body.C_TITLE}'`;
+        return (await con).execute( sql );
+    },
+    modify : async ( body )=>{
+                const sql = `update Cal set C_TITLE='${body.C_TITLE}', C_CONTENT='${body.C_content}',C_STARTDATE=sysdate , C_ENDDATE=sysdate where C_ID=${body.C_ID}`;
+                return (await con).execute( sql );
+}
+}
+module.exports={calRead, calInsert, calUpdate}
--- src/router/router.js
+++ src/router/router.js
@@ -12,13 +12,13 @@ module.exports=(app)=>{
     const memberRouter = require("./member/member_router")
     const todoRouter=require("./todo/todo_router")
     const boardRouter = require("./board/board_router");
-    // const calRouter=require('./calendar/calendar_router');
-
+    const calRouter= require("./calendar/calendar_router")
 
 
     app.use("/member", memberRouter)
     app.use("/todo",todoRouter)
     app.use("/board", boardRouter)
+    app.use("/calendar", calRouter)
 
     const todoCtrl=require('../controller/todo/todo_ctrl');
     // router.post('/main',(req,res)=>{
@@ -27,7 +27,8 @@ module.exports=(app)=>{
     //     res.render('index',{todo:todo_ctrl.views.list() ,boardList:boardList})
     // })
 
-
+    const calCtrl= require('../controller/calendar/calendar_ctrl');
+    router.get('/main', calCtrl.views.data)
     router.get('/main', todoCtrl.views.data)
 
     router.get("/database", async(req,res) =>{
@@ -35,9 +36,6 @@ module.exports=(app)=>{
         connection = await getConnection();
         const result = await connection.execute("select * from ALLUSER")
         res.json(result.rows);
-    })
-    router.get('/calendar',(req,res)=>{
-        res.render('calendar')
     })
     //캘린더 라우터 아직 만들어지지 않아서 그대로 두겠습니다.
     //이후 캘린더 라우터 만들어지면 옮기겠습니다.
@@ -50,8 +48,38 @@ module.exports=(app)=>{
     router.get('/',(req,res)=>{
         res.render('login', {id : req.session.username})
     })
+    const todolist=[
+        {
+            title:'프로젝트 마무리 하기',
+            priority: '1',
+            pk:'aaa'
+        },
+        {
+            title:'체크박스 함께 움직임',
+            priority: '2',
+            pk:'bbb'
+        },
+        {
+            title:'수정 필요',
+            priority: '0',
+            pk:'ccc'
+
+        }
+    ];
+    const boardList=[{
+        title:'boardtitle',
+        url:'aaa'
+    }];
+
+    const todo_ctrl=require('../controller/todo/todo_ctrl')
+    router.post('/main',(req,res)=>{
+        res.render('index',{todo:todolist,boardList:boardList})
+    })
+
+    router.get('/main',(req,res)=>{
+        res.render('index',{todo:todolist,boardList:boardList})
+    })
+
 
      return router;
 }
-
-
